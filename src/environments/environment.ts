c990e162f33fee9483f2db197b97dac655da8260
Daniel Jabry <daniel.jabry@rezonence.com>
2018-06-10 02:34:53 +0100

    Updated to latest identity pool id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  identityPoolId: "eu-west-1:2ebdfee0-ad4e-4a03-9aec-e03dccf8f72d",
+  identityPoolId: "eu-west-1:73285d9d-e1cf-47d1-9b42-ac05c91de986",
   googleApiKey: "AIzaSyBF463pZSwavuoLSqeqaF_Hd7EpipG7gWk",
   region: "eu-west-1"
 };
